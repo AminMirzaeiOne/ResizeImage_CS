7cdcc783ee6f86489b430dc18bbb30b0eea2ca0b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-25 00:37:48 +0330

    - Create standardSize Filed In Resize Class

--- ResizeImage/Resize.cs
+++ ResizeImage/Resize.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,11 @@ namespace ResizeImage
         {
             Custom, Default, VerySmall, Small, Medium, Large, VeryLarge
         }
+
+        private ResizeImage.Resize.StandardSizes standardSize = ResizeImage.Resize.StandardSizes.Custom;
+
         public System.Int32 Wdith { get; set; } = 50;
         public System.Int32 Height { get; set; } = 50;
+
     }
 }
